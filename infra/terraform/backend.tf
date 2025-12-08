0dbe1b0feb4e3a1eac6d9d0d27cfa3d5956e9497
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2025-12-08 15:27:21 +0100

    refactor: Update Terraform backend configuration to use lockfile instead of DynamoDB for state locking.

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "leothatguy-s3-hng" # REPLACE WITH YOUR BUCKET NAME
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "leothatguy-hng-devops"            # REPLACE WITH YOUR DYNAMODB TABLE
+    use_lockfile   = true
     encrypt        = true
   }
 }
